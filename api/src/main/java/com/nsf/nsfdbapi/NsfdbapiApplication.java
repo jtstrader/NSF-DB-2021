f7fdbf703751c3de1202cb744a3e61946da27498
Jacob Strader <1106561@live.mercer.edu>
2021-09-23 17:49:52 -0400

    bang

---
+++ api/src/main/java/com/nsf/nsfdbapi/NsfdbapiApplication.java
@@ -0,0 +1,13 @@
+package com.nsf.nsfdbapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NsfdbapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NsfdbapiApplication.class, args);
+	}
+
+}
---
+++ api/src/test/java/com/nsf/nsfdbapi/NsfdbapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nsf.nsfdbapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NsfdbapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
