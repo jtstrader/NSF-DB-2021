0ddd030856f636bb56f595c7d191d01987c789e4
narumzj <60524569+narumzj@users.noreply.github.com>
2021-09-25 02:52:11 -0400

    Start of creating Classes. Created temporary MenuBar.

---
+++ gui/src/main/java/com/nsfdb/gui/dashboard/FamilyTreePanel.java
@@ -0,0 +1,13 @@
+package com.nsfdb.gui.dashboard;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class FamilyTreePanel extends JPanel {
+    public FamilyTreePanel() {
+        Dimension windowSize = new Dimension(600, 600);
+        this.setPreferredSize(windowSize);
+        this.setMaximumSize(windowSize);
+        this.setMinimumSize(windowSize);
+    }
+}
---
+++ gui/src/main/java/com/nsfdb/gui/dashboard/MonkeyDetailsPanel.java
@@ -0,0 +1,13 @@
+package com.nsfdb.gui.dashboard;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class MonkeyDetailsPanel extends JPanel {
+    public MonkeyDetailsPanel() {
+        Dimension windowSize = new Dimension(600, 600);
+        this.setPreferredSize(windowSize);
+        this.setMaximumSize(windowSize);
+        this.setMinimumSize(windowSize);
+    }
+}
