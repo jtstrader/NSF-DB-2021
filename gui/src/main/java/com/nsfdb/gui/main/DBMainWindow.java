0825cc46c4014170adb63fdabcd664a5e3b28393
narumzj <60524569+narumzj@users.noreply.github.com>
2021-10-02 13:51:42 -0400

    Start of creating Classes. Created temporary MenuBar.

--- gui/src/main/java/com/nsfdb/gui/main/DBMainWindow.java
+++ gui/src/main/java/com/nsfdb/gui/main/DBMainWindow.java
@@ -3,6 +3,7 @@ package com.nsfdb.gui.main;
 import com.nsfdb.gui.dashboard.*;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.KeyEvent;
 
@@ -51,7 +52,7 @@ public class DBMainWindow extends JFrame {
         window.setTitle("Monkey DB");
 
         FamilyTreePanel treePanel = new FamilyTreePanel();
-        window.add(treePanel);
+        window.add(treePanel, BorderLayout.CENTER);
 
         window.pack();
         window.setVisible(true);
