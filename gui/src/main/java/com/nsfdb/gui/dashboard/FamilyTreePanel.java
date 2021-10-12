dbfe2dd72825e9be5989bbc786955e543c2e7a6e
narumzj <60524569+narumzj@users.noreply.github.com>
2021-10-12 18:57:05 -0400

    Added tree to TreePanel

--- gui/src/main/java/com/nsfdb/gui/dashboard/FamilyTreePanel.java
+++ gui/src/main/java/com/nsfdb/gui/dashboard/FamilyTreePanel.java
@@ -1,17 +1,46 @@
 package com.nsfdb.gui.dashboard;
 
 import javax.swing.*;
+import javax.swing.tree.DefaultMutableTreeNode;
 import java.awt.*;
 
 public class FamilyTreePanel extends JPanel {
+    private JTree tree;
+
     public FamilyTreePanel() {
+        super(new GridLayout(1,0));
         Dimension windowSize = new Dimension(300, 400);
         this.setPreferredSize(windowSize);
         this.setMaximumSize(windowSize);
         this.setMinimumSize(windowSize);
-        this.setBackground(Color.GRAY);
 
-        JLabel treeLabel = new JLabel("Tree Place Holder Panel");
-        add(treeLabel);
+        DefaultMutableTreeNode top = new DefaultMutableTreeNode("Monkeys");
+
+        viewTest(top); // Tests the tree and the scroll feature
+
+        tree = new JTree(top);
+
+        JScrollPane treeView = new JScrollPane(tree);
+        this.add(treeView);
+    }
+
+    // Fills the tree as a test
+    private void viewTest(DefaultMutableTreeNode top) {
+        DefaultMutableTreeNode sec =
+                new DefaultMutableTreeNode("Second");
+        top.add(sec);
+        DefaultMutableTreeNode third =
+                new DefaultMutableTreeNode("Third");
+        sec.add(third);
+        DefaultMutableTreeNode fourth =
+                new DefaultMutableTreeNode("Fourth");
+        third.add(fourth);
+        DefaultMutableTreeNode Fifth =
+                new DefaultMutableTreeNode("Fifth");
+        fourth.add(Fifth);
+
+        for(int i = 0; i <= 100; i++) {
+            top.add(new DefaultMutableTreeNode("Filler"));
+        }
     }
 }
