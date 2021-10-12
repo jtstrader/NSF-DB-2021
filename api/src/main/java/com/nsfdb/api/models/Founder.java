a0b3937bce9b25e0219d91655831a1c5b1bc0dc0
Jacob Strader <11006561@live.mercer.edu>
2021-10-11 22:42:42 -0400

    updated to match database table name

--- api/src/main/java/com/nsfdb/api/models/Founder.java
+++ api/src/main/java/com/nsfdb/api/models/Founder.java
@@ -4,7 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 import java.sql.Date;
 
-@Entity(name="CSFounders")
+@Entity(name="CSFounder")
 public class Founder {
     @Id
     String MCode;
