30e0c4a297b5327e4c2b8cf8e614cefdec853665
Gabriel Bryant <10997526@live.mercer.edu>
2021-10-21 12:44:55 -0400

    changed monkey and founder models, controllers, etc. in order to allow for retrievel of single entities from the database not just lists.

--- api/src/main/java/com/nsfdb/api/models/Monkey.java
+++ api/src/main/java/com/nsfdb/api/models/Monkey.java
@@ -1,5 +1,7 @@
 package com.nsfdb.api.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -7,6 +9,7 @@ import javax.persistence.Id;
 import java.sql.Date;
 
 @Entity(name="CSSubject")
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 public class Monkey {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
