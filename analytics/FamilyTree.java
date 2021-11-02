1044fb72ff15f85eb9e8ac79c64b06980d9ad99c
Spymines <66049580+Spymines@users.noreply.github.com>
2021-11-02 14:27:56 -0400

    Added Family Tree Array List
    Still needs custom queries

---
+++ analytics/FamilyTree.java
@@ -0,0 +1,42 @@
+package com.nsfdb.analytics.FamilyTree;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+import com.nsfdb.analytics.FamilyTree.*;
+import com.nsfdb.api.*;
+
+public class FamilyTree {
+    private List<Monkey> MonkeyList;
+    private RestClient client;
+
+    public FamilyTree()
+    {
+        this.client = new RestClient("http://localhost:8080");
+    }
+
+    public create(){
+        private List<Monkey> founders = client.get("api/founder");
+
+        for(int i = 0, i < founders.size(), i++){
+            MonkeyList.add(founders.get(i));
+            addChain(founders.get(i).tattoo);
+        }
+
+        return MonkeyList;
+    }
+
+    private addChain(string behavior_mom){
+        private List<Monkey> children = client.get("api/monkey/" + behavior_mom);
+        if(children.size() == 0){
+            break;
+        }
+
+        for(int i = 0; i < children.size(), i++){
+            MonkeyList.add(children.get(i))
+            addChain(children.get(i).animalid);
+        }
+
+    }
+}
