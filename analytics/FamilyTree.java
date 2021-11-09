3c2e35122e554b3cfa4ddd9e60bb4c1a2ce9bae7
Mackenzie Stroup <83418612+Mstroup0@users.noreply.github.com>
2021-11-09 13:14:51 -0500

    Added Comments To the code.

--- analytics/FamilyTree.java
+++ analytics/FamilyTree.java
@@ -19,7 +19,11 @@ public class FamilyTree {
         this.mapper = new ObjectMapper();
         this.MonkeyList = new ArrayList<Monkey>();
     }
-
+/*
+    The create method will build an array list of Monkeys in a for loop. Starting with a founder
+    and then adding all of its children, with the addChain. Then it will move on to the next founder and repeat.
+    It will do this process for all Founders and Monkeys.
+ */
     public ArrayList<Monkey> create(){
         private ArrayList<Founder> founders = mapper.readValue(client.get("api/founder"),new TypeReference<ArrayList<Founder>>(){});
 
@@ -30,7 +34,12 @@ public class FamilyTree {
 
         return MonkeyList;
     }
-
+/*
+    The addChain method is an recursive method. The method builds an array list of the children for a mom.
+    For each mom it checks to so if the there are any children if there is not it will break out of the method.
+    If there are children then it will add them to the Main array List and then use the recursive call to check
+    that Monkey/Child to see if they have any Children.
+ */
     private void addChain(string behavior_mom){
         private ArrayList<Monkey> children = mapper.readValue(client.get("api/monkey/mom/" + behavior_mom),new TypeReference<ArrayList<Monkey>>(){});
         if(children.size() == 0){
@@ -42,7 +51,12 @@ public class FamilyTree {
             addChain(children.get(i).animalid);
         }
     }
-
+/*
+    The founderToMonkey method adds the Founder monkey to the Monkey Class.
+    In this method the pegidree is set to 1 and the gender is Female.
+    Then informations from the Founder's table to set to corrisponding data in the Monkey Table.
+    This is done so the founders can be includes in the same list as the Monkeys
+ */
     private Monkey founderToMonkey(founder monk){
         Monkey newMonkey = new Monkey();
 
