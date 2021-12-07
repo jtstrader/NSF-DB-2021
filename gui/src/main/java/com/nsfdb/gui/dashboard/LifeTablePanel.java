51a0b95464b0c3794e9b93aa8621086d3d06439d
narumzj <60524569+narumzj@users.noreply.github.com>
2021-12-07 13:26:33 -0500

    Added comments

--- gui/src/main/java/com/nsfdb/gui/dashboard/LifeTablePanel.java
+++ gui/src/main/java/com/nsfdb/gui/dashboard/LifeTablePanel.java
@@ -10,11 +10,13 @@ public class LifeTablePanel extends JPanel {
     JTable lifeTable;
 
     public LifeTablePanel() throws JsonProcessingException {
+        // Gets the columns and data Arrays from the LifeTable class and adds it to a JTable
         LifeTable tableData = new LifeTable();
         String columns[] = tableData.getColumns();
         double data[][] = tableData.getLifeTableData();
         Double dataBoxed[][] = new Double[data.length][data[0].length];
 
+        // Boxes the array type double into array type Double for use in the JTable
         for(int i = 0; i < data.length; i++){
             for(int j = 0; j < data[0].length; j++){
                 dataBoxed[i][j] = data[i][j];
@@ -25,6 +27,7 @@ public class LifeTablePanel extends JPanel {
         this.add(sp);
     }
 
+    // Test the LifeTablePanel class by creating a JFrame and adding a LifeTablePanel to it
     public static void main(String[] args) throws JsonProcessingException {
         JFrame f = new JFrame();
 
